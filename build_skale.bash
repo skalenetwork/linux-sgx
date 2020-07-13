07c74c0218686b3201f5ece243300612e7b5760a
kladko <13399135+kladkogex@users.noreply.github.com>
2020-07-13 17:31:12 +0300

    Adding crypto

--- build_skale.bash
+++ build_skale.bash
@@ -1,2 +1,5 @@
 docker build --target builder -f docker/build/DockerfileSKALE .
-#sgx_linux_x64_sdk_2.10.100.2.bin
+docker run  IMAGE_NAME
+docker cp CONTAINER_NAME:/linux-sgx/linux/installer/bin/sgx_linux_x64_sdk_2.10.100.2.bin /tmp
+
+
